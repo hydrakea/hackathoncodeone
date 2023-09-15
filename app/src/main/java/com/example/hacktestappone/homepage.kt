0a602595da5aa50dc410b35af076acb075659400
Harsha Krishna Divate <harshdivate.hd@gmail.com>
2023-09-15 09:37:41 +0530

    homepage.ui

---
+++ app/src/main/java/com/example/hacktestappone/homepage.kt
@@ -0,0 +1,12 @@
+package com.example.hacktestappone
+import com.example.hacktestappone.homepage
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class homepage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_homepage)
+    }
+}
