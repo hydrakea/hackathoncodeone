7382b92a51f89b43aab5fdac2e357336288a8f97
Harsha Krishna Divate <harshdivate.hd@gmail.com>
2023-09-15 12:41:16 +0530

    homepage.ui2

--- app/src/main/java/com/example/hacktestappone/homepage.kt
+++ app/src/main/java/com/example/hacktestappone/homepage.kt
@@ -1,12 +1,15 @@
 package com.example.hacktestappone
+import android.animation.ObjectAnimator
 import com.example.hacktestappone.homepage
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 
 class homepage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_homepage)
+
     }
 }
